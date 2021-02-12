33afa085a794d7ba8730ee0a7bf70916f34dec5a
Lazzat <lazzat.meirambekova@gmail.com>
2021-02-12 19:43:47 +0600

    Проверка 500

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const router = require('express').Router(); // создали роутер
 
-const {getUsers, getProfile} = require('../controlers/users');
-
+const { getUsers, getProfile } = require('../controlers/users');
 
 router.get('/users', getUsers);
 router.get('/users/:id', getProfile);
